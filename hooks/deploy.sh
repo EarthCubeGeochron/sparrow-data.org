b8ebf4a7de59d125a81fb6110b7ea2103a96f1d2
Daven Quinn <dev@davenquinn.com>
2020-05-21 15:53:16 -0500

    Try to add basic auth

--- hooks/deploy.sh
+++ hooks/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd /code
+
+echo $GITHUB_TOKEN | docker login docker.pkg.github.com -u $GITHUB_ACTOR --password-stdin
 docker-compose build --pull frontend
 docker-compose up --build --remove-orphans -d frontend
