555f1cdbe78ce595d9a5a859c26a1e0d5909ecf8
Daven Quinn <dev@davenquinn.com>
2022-07-12 02:26:50 -0500

    Added a test lab configuration

---
+++ test-lab/backend-plugins/__init__.py
@@ -0,0 +1,40 @@
+import sparrow
+from sparrow.plugins import SparrowPlugin
+from sparrow.database import User, on_conflict
+from json import load
+from os import path
+
+_test_data = path.abspath(path.join(path.dirname(__file__), "project-dump.json"))
+
+
+class TestDataInitPlugin(SparrowPlugin):
+    """A plugin to initialize a test dataset on each app startup"""
+
+    name = "test-init"
+
+    def on_database_ready(self, db):
+        """Loads testing data and create user on app startup."""
+        # Create a user directly in the database
+        with on_conflict("do-nothing"):
+            user = User(username="Test")
+            user.set_password("Test")
+            db.session.add(user)
+            db.session.commit()
+
+        ## TODO: running importers on app lifecycle methods is a bad idea,
+        # because we might run the method on several workers in production.
+        db = self.app.database
+        project_count = db.session.query(db.model.project).count()
+        if project_count > 0:
+            return
+        db = self.app.database
+        data = load(open(_test_data, "r"))
+        db.load_data("project", data["data"])
+
+
+@sparrow.task()
+def log_sessions():
+    db = sparrow.get_database()
+    sessions = db.session.query(db.model.session).all()
+    for session in sessions:
+        print(session.uuid)
